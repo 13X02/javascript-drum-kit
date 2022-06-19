eac2ac6856861500520425e57f422d89b46cfcd7
Abhijith H <abhijithh496@gmail.com>
2022-06-19 20:04:24 +0530

    key events added

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const window = document.querySelector('window');
 window.addEventListener('keydown',function(e){
-    console.log(e);
+    const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    console.log(audio);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
 });
